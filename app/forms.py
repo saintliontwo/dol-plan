ae6e5a63e5ebc1e077d47e42672ff974a621ae00
Ivan <saintliontwo@gmail.com>
2020-02-23 12:29:17 +0200

    need to realize errors messages in templates

--- app/forms.py
+++ app/forms.py
@@ -4,9 +4,11 @@ from wtforms.fields.html5 import DateField, IntegerField
 
 
 class MyForm(FlaskForm):
-    main_event = StringField('Центральное мероприятие:', [validators.Length(min=4, max=32, message="Укажите название центрального мероприятия"),
-                                                          validators.DataRequired()])
-    period = IntegerField('Номер смены:', [validators.NumberRange(min=1, max=4, message="Введите номер смены с 1 по 4"),
-                                           validators.DataRequired()])
+    main_event = StringField('Центральное мероприятие:', [validators.Length(min=4, max=32, ),
+                                                          validators.DataRequired(message="Название центрального "
+                                                                                          "мероприятия должно быть "
+                                                                                          "длинее 4 символов")])
+    period = IntegerField('Номер смены:', [validators.NumberRange(min=1, max=4, ),
+                                           validators.DataRequired(message="Введите номер смены с 1 по 4")])
     date = DateField('Дата начала смены:', [validators.DataRequired()], format='%Y-%m-%d')
     submit = SubmitField('Подтвердить')
